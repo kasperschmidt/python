b74d142d789d39789821065e77e9ba0d9ffc150a
Kasper Schmidt <kbschmidt@aip.de>
2016-01-29 08:37:32 +0100

    Adjusted mock GLASS ids and DS9 regions

--- manipulate_3DHSTdata.py
+++ manipulate_3DHSTdata.py
@@ -8,7 +8,7 @@ import pyfits
 import numpy as np
 import manipulate_3DHSTdata as m3d
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-def rename_3DHST2GLASS(outputdirectory,id,id_GLASSmock=9999,
+def rename_3DHST2GLASS(outputdirectory,id,id_GLASSmock=999,
                        parentdatadir='/Volumes/DATABCKUP3/GOODSdata/3D-HST/GOODSS_WFC3_V4.1.5/',
                        clobber=False,testing=False,verbose=True):
     """
@@ -18,7 +18,8 @@ def rename_3DHST2GLASS(outputdirectory,id,id_GLASSmock=9999,
     --- INPUT ---
     outputdirectory     Name of directory where the renamed files will be put
     id                  3D-HST ID of object to rename files for (including cluster name, e.g.,
-    id_GLASSmock        Mock GLASS id to assign to object (GiG and GiGz expect id on the format _0xxxx)
+    id_GLASSmock        Mock GLASS id to assign to object (GiG and GiGz expect id on the format %.5d)
+                        Use IDs < 1000 to enable including the 3D-HST field number in the ID.
     parentdatadir       Top level directory of 3D-HST data
     clobber             Overwrite files in directory?
     verbose             Toggle verbosity
@@ -49,12 +50,12 @@ def rename_3DHST2GLASS(outputdirectory,id,id_GLASSmock=9999,
         os.makedirs(outputdirectory)
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     for fn in filenames:
-        fnbase = fn.split('_'+str(id))[0]
-        fnext  = fn.split('_'+str(id))[1]
-
+        fnbase           = fn.split('_'+str(id))[0]
+        fnext            = fn.split('_'+str(id))[1]
         field, field_no  = fn.split('/')[-4].split('-')
+        IDnew            = str("%.3d" % id_GLASSmock)+str("%.2d" % int(field_no))
 
-        GLASSnamebase = field.upper()+'3DHST-555-'+str("%.5d" % int(field_no))+'_'+str("%.5d" % id_GLASSmock)+'-G141'
+        GLASSnamebase = field.upper()+'3DHST-555-'+str("%.5d" % int(field_no))+'_'+IDnew+'-G141'
 
         if fnext.endswith('.png'):
             if fn.endswith(str(id)+'.1D.png'):
@@ -77,7 +78,7 @@ def rename_3DHST2GLASS(outputdirectory,id,id_GLASSmock=9999,
             if cpout != '': print cpout
 
             if '.2D.fits' not in cpcmd:
-                cpcmd = cpcmd.replace(str("%.5d" % id_GLASSmock)+'-G141',str("%.5d" % id_GLASSmock)+'-G102')
+                cpcmd = cpcmd.replace(IDnew+'-G141',IDnew+'-G102')
                 cpout = commands.getoutput(cpcmd)
                 if cpout != '': print cpout
 
@@ -98,20 +99,41 @@ def caruanaLAEs(outputdir,verbose=True):
     m3d.caruanaLAEs(outdir,verbose=True)
 
     """
-    ID_skelton    = np.array([25614,16492,19953,19906,16007,15419,17484,15601,19546,12210])
-    ID_GLASSmock  = np.array([9901 ,9902 ,9903 ,9904 ,9905 ,9906 ,9907 ,9908 ,9909 ,9910 ])
-    Lyawaves      = np.array([7111.10742188,6935.99023438,6684.72607422,6684.72607422,6545.99560547,
-                              6323.49023438,5762.24121094,5192.36279297,5055.98535156,4980.97900391])
-    redshifts     = Lyawaves/1216.-1.
-
-
+    ID_skelton, ID_GLASSmock, Lyawaves, redshifts = m3d.caruanaLAE_info()
 
     for oo, obj in enumerate(ID_skelton):
         if verbose: print '\n - Copying/renaming data of object '+str(obj)+'/"ID_GLASS"='+\
                           str(ID_GLASSmock[oo])+' (Lya at z = '+str("%.2f" % redshifts[oo])+')'
         obj_files = m3d.rename_3DHST2GLASS(outputdir,ID_skelton[oo],id_GLASSmock=ID_GLASSmock[oo],
                                            testing=False,clobber=True,verbose=verbose)
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+def caruanaLAE_info():
+    """
+    Retuning ingormation about the Caruane LAEs
 
+    """
+    ID_skelton    = np.array([25614,16492,19953,19906,16007,15419,17484,15601,19546,12210])
+    ID_GLASSmock  = np.array([1    ,2    ,3    ,4    ,5    ,6    ,7    ,8    ,9    ,10   ])
+    Lyawaves      = np.array([7111.10742188,6935.99023438,6684.72607422,6684.72607422,6545.99560547,
+                              6323.49023438,5762.24121094,5192.36279297,5055.98535156,4980.97900391])
+    redshifts     = Lyawaves/1216.-1.
+
+    return  ID_skelton, ID_GLASSmock, Lyawaves, redshifts
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+def generateCaruanaLAEregions(verbose=True):
+    """
+
+    --- EXAMPLE OF USE ---
+    import manipulate_3DHSTdata as m3d
+    m3d.generateCaruanaLAEregions()
+
+    """
+    outputpath    = './CaruanaLAEs_data/'
+    ID_skelton, ID_GLASSmock, Lyawaves, redshifts = m3d.caruanaLAE_info()
+
+    for ll, lw in enumerate(Lyawaves):
+        outputfile    = str("%.3d" % ID_GLASSmock[ll])+'_emissionlines.reg'
+        m3d.create_ELds9region(lw, outname=outputfile, outpath=outputpath)
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 def create_ELds9region(lyawave,outname='ds9region_emissionlines.reg',outpath='./',verbose=True):
     """
@@ -123,31 +145,40 @@ def create_ELds9region(lyawave,outname='ds9region_emissionlines.reg',outpath='./
 
     --- EXAMPLE OF USE ---
     import manipulate_3DHSTdata as m3d
-    m3d.create_ELds9region(9901.0,verbose=True)
+    create_ELds9region(7111.10742188)
 
     """
     zobj      = lyawave/1216.-1.
     zobj_low  = lyawave/3726.-1.
     if verbose: print ' - Generating region file for object at z='+str("%.2f" % zobj)+\
-                      ' (or z='+str("%.2f" % zobj_low)+')'
+                      ' (or z='+str("%.2f" % zobj_low)+'):\n   '+outpath+outname
 
     oiiiwave  = 5007.*(zobj_low+1.)
-    civwave   = XX1246.*(zobj+1.)
+    civwave   = 1549.*(zobj+1.)
     ciiiwave  = 1909.*(zobj+1.)
-
+    mgiiwaveH  = 2788.*(zobj+1.)
+    mgiiwaveL  = 2788.*(zobj_low+1.)
+    halphawave = 6563.*(zobj_low+1.)
 
     regtext = """
 # Region file format: DS9 version 4.1
 global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1
 wcs;
-circle(%s,0.0,200) # color=red width=2 text={Lya @ z~%s (or [OII] @ z~%s)}
-circle(%s,0.0,200) # color=red width=2 text={[OIII] @ z~%s}
-circle(%s,0.0,200) # color=red width=2 text={CIV @ z~%s}
-circle(%s,0.0,200) # color=red width=2 text={CIII] @ z~%s}
-""" % (lyawave,zobj,zobj_low,
-       oiiiwave,zobj_low,
-       civwave,zobj,
-       ciiiwave,zobj)
+circle(%s,0.0,200) # color=green width=2 text={Lya @ z~%s (or [OII] @ z~%s)}
+circle(%s,0.0,200) # color=magenta width=2 text={[OIII] @ z~%s}
+circle(%s,0.0,200) # color=green width=2 text={CIV @ z~%s}
+circle(%s,0.0,200) # color=green width=2 text={CIII] @ z~%s}
+circle(%s,0.0,200) # color=green width=2 text={MgII @ z~%s}
+circle(%s,0.0,200) # color=magenta width=2 text={MgII @ z~%s}
+circle(%s,0.0,200) # color=magenta width=2 text={Halpha @ z~%s}
+""" % (lyawave,  str("%.2f" % zobj),        str("%.2f" % zobj_low),
+       oiiiwave, str("%.2f" % zobj_low),
+       civwave,  str("%.2f" % zobj),
+       ciiiwave, str("%.2f" % zobj),
+       mgiiwaveH, str("%.2f" % zobj),
+       mgiiwaveL, str("%.2f" % zobj_low),
+       halphawave, str("%.2f" % zobj_low)
+       )
 
     fout = open(outpath+outname,'w')
     fout.write(regtext)
