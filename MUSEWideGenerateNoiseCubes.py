0e5d3d54e7607dd1e43018f3a8545a723e517ce4
Kasper Schmidt <kbschmidt@aip.de>
2017-06-19 10:25:12 +0200

    Generate variance cubes from bgrstat files

--- MUSEWideGenerateNoiseCubes.py
+++ MUSEWideGenerateNoiseCubes.py
@@ -8,6 +8,8 @@ import pdb
 def create_variancecube(effvariancefile,exposurecubefile,ext_var=0,ext_exp='EXP',Nexpmax=4.0,
                         save_cube=True,clobber=False,verbose=True):
     """
+    ---> use create_variancecube_bgrstat <---
+
     Generate a variance cube by scaling the effective variance by the exposure map
 
     NB: Generates variance cube from EFFNOISE*tables
@@ -58,6 +60,8 @@ def create_variancecube(effvariancefile,exposurecubefile,ext_var=0,ext_exp='EXP'
 def create_variancecube_datapixstat(effvariancefile,exposurecubefile,ext_pixstat=1,col_var='mean',var_unitscale=1000,
                                     ext_exp='EXP',Nexpmax=4.0,save_cube=True,clobber=False,verbose=True):
     """
+    ---> use create_variancecube_bgrstat <---
+
     Generate a variance cube by scaling the effective variance by the exposure map
 
     NB: Generates variance cube from datapixstat*tables
@@ -104,6 +108,64 @@ def create_variancecube_datapixstat(effvariancefile,exposurecubefile,ext_pixstat
         return varfilename
     else:
         return var_cube
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+def create_variancecube_bgrstat(effstatfile,exposurecubefile,ext_pixstat=1,col_sig='effsig',
+                                    ext_exp='EXP',save_cube=True,clobber=False,verbose=True):
+    """
+    Generate a variance cube by scaling the effective standard noise by the exposure map
+
+    NB: Generates variance cube from stat files in /store/data/musewide/bgrstat.tgz
+
+        The STAT extension of /store/data/musewide/candels-*/DATACUBE_*_v1.0_dcbgc_effnoised.fits on arche
+        contains this noise cube generated.
+
+    --- INPUT ---
+
+    --- EXAMPLE OF USE ---
+    import MUSEWideGenerateNoiseCubes as mgnc
+    exposurecubefile = '/Volumes/DATABCKUP2/MUSE-Wide/datacubes_dcbgc/DATACUBE_candels-cdfs-02_v1.0_dcbgc.fits'
+    effstatfile      = '/Volumes/DATABCKUP2/MUSE-Wide/effective_noise/bgrstat/candels-cdfs-02/candels-cdfs-02_bgrstat.fits'
+    cube             = mgnc.create_variancecube_bgrstat(effstatfile,exposurecubefile)
+
+    """
+    if verbose: print ' - Loading effective variance and exposure map'
+    datapixstat_table   = pyfits.open(effstatfile)[ext_pixstat].data
+    var_eff             = datapixstat_table[col_sig]**2.0
+    var_lambda          = datapixstat_table['wave']
+
+    exposure_map        = pyfits.open(exposurecubefile)[ext_exp].data
+    cube_shape          = exposure_map.shape
+
+    Nexpmax             = np.float(np.max(exposure_map[np.isfinite(exposure_map)]))
+
+    if verbose: print ' - Checking wavelength dimensions (found '+str(len(var_lambda))+' wavelength slices in effective variance)'
+    if len(var_eff) == cube_shape[0]:
+        if verbose: print '   The number of wavelength slices agrees between the exposure map and the effective variance'
+    else:
+        sys.exit(' ---> The number of wavelength slices does not agree between the exposure map and the effective variance')
+
+    if verbose: print ' - Building cube of effective variance'
+    var_eff_cube        = np.repeat( np.repeat(var_eff[:, np.newaxis],cube_shape[1],axis=1)[:,:,np.newaxis],cube_shape[2],axis=2)
+
+    if verbose: print ' - Scaling effective variance by exposure map (masking 0s in exposure map and using NmaxExp = '+\
+                      str(Nexpmax)+')'
+    mask_zeros          = (exposure_map == 0)
+
+    inv_exp_map_masked  = np.ma.array(Nexpmax/exposure_map,mask=mask_zeros)
+    var_eff_cube_masked = np.ma.array(var_eff_cube,mask=mask_zeros)
+
+    var_cube_masked     = np.multiply(inv_exp_map_masked, var_eff_cube_masked)
+    var_cube            = var_cube_masked.filled(fill_value=np.nan)
+
+    if save_cube:
+        varfilename     = effstatfile.replace('.fits','_variancecube.fits')
+        exposure_hdr    = pyfits.open(exposurecubefile)[ext_exp].header
+        mgnc.save_cube(var_cube,exposure_hdr,varfilename,clobber=clobber,verbose=verbose)
+        return varfilename
+    else:
+        return var_cube
+
+
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 def save_cube(var_cube,exposure_hdr,varfilename,clobber=False,verbose=True):
     """
