703ae28de86a844f608991dd3a12bfeaf2e6e596
Kasper Schmidt <kbschmidt@aip.de>
2020-01-23 16:12:46 +0100

    Set line list to UV for z=0.0 spectra in plot

--- MUSEWidePlots.py
+++ MUSEWidePlots.py
@@ -1123,6 +1123,8 @@ def plot_1DspecOverview(spectra, labels, wavecols, fluxcols, fluxerrcols, redshi
     if linenames is None:
         if redshift > 2.5:
             linenames    = ['Lyb','Lya','CII','SiIVOIV','CIV','HeII','CIII','MgII','OII']
+        elif redshift == 0.0:
+            linenames    = ['Lyb','Lya','CII','SiIVOIV','CIV','HeII','CIII','MgII','OII']
         elif (redshift > 1.5) & (redshift <= 2.5):
             linenames    = ['CIV','HeII','CIII','MgII','OII','NeIII','Hg','Hb','OIII']
         else:
