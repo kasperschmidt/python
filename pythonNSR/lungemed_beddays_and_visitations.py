881f79092eeafdc2dcb6e68dae3151b15876a58d
Kasper Borello Schmidt <kasperschmidt@me.com>
2022-01-11 15:58:54 +0100

    plotting visitation counts

--- pythonNSR/lungemed_beddays_and_visitations.py
+++ pythonNSR/lungemed_beddays_and_visitations.py
@@ -406,8 +406,283 @@ def plot_perday_occupancy(measurehours=[23], loaddatafile='lungemedLPR3dataframe
     plt.close('all')
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    plotname = 'visitationcounts.pdf'
+    if verbose: print(' - Initiating '+plotname)
+
+    fig = plt.figure(figsize=(9, 6))
+    fig.subplots_adjust(wspace=0.1, hspace=0.1, left=0.1, right=0.97, bottom=0.10, top=0.90)
+    Fsize = 10
+    lthick = 2
+    marksize = 4
+    plt.rc('text', usetex=False)
+    plt.rc('font', family='serif', size=Fsize)
+    plt.rc('xtick', labelsize=Fsize)
+    plt.rc('ytick', labelsize=Fsize)
+    plt.clf()
+    plt.ioff()
+    #plt.title('Henvisninger til lungemed. NAE',fontsize=Fsize)
+
+    xvalues = df_results['dates_'+str(measurehour)]
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.YearLocator())
+
+    xerr = None
+    yerr = None
+
+    plt.grid(linestyle=':', linewidth=lthick/2.)
+    # --------- RANGES ---------
+    ymin = -5.0
+    ymax = 10.0
+    dy   = ymax - ymin
+    plt.ylim([ymin,ymax])
+
+    # --------- COLORMAP ---------
+    cmap = plt.cm.get_cmap('viridis')
+    #cmap = plt.cm.get_cmap('plasma')
+    cmin = 0
+    cmax = 100
+
+    colnorm = matplotlib.colors.Normalize(vmin=cmin, vmax=cmax)
+    cmaparr = np.linspace(cmin, cmax, num=50)
+    m = plt.cm.ScalarMappable(cmap=cmap)
+    m.set_array(cmaparr)
+    #cb = plt.colorbar(m)
+    #cb.set_label('belægningsprocent')
+
+    # --------- POINT AND CURVES ---------
+    pointcolor = cmap(colnorm(10))
+    plt.errorbar(xvalues, df_results['count_vis_aka_23'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Akut afdelingen')
+
+    pointcolor = cmap(colnorm(35))
+    plt.errorbar(xvalues, df_results['count_vis_lungNAE_23'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Lungemed NAE (egen afd.)')
+
+    pointcolor = cmap(colnorm(60))
+    plt.errorbar(xvalues, df_results['count_vis_lungSLA_23'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Lungemde. SLA')
+
+    pointcolor = cmap(colnorm(85))
+    plt.errorbar(xvalues, df_results['count_vis_other_23'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Andre afdelinger')
+
+    lineymin = ymin + dy*0.24
+    lineymax = ymin + dy*0.40
+    textymin = ymin + dy*0.05
+    textymax = ymin + dy*0.10
+    plt.plot([datetime.datetime.strptime("10-06-2021", "%d-%m-%Y"), datetime.datetime.strptime("10-06-2021", "%d-%m-%Y")],
+             [lineymin, lineymax], '-', color='gray', lw=lthick, zorder=5)
+    plt.text(datetime.datetime.strptime("10-06-2021", "%d-%m-%Y"), textymin, '10-06-2021', fontsize=Fsize, rotation=90, color='gray',
+             horizontalalignment='center', verticalalignment='bottom')
+
+    plt.plot([datetime.datetime.strptime("01-03-2021", "%d-%m-%Y"), datetime.datetime.strptime("01-03-2021", "%d-%m-%Y")],
+             [lineymin, lineymax], '-', color='gray', lw=lthick, zorder=5)
+    plt.text(datetime.datetime.strptime("01-03-2021", "%d-%m-%Y"), textymin, '01-03-2021', fontsize=Fsize, rotation=90, color='gray',
+             horizontalalignment='center', verticalalignment='bottom')
+
+    # --------- LABELS ---------
+    plt.xlabel('Dato for måling')
+    plt.ylabel('Henvisninger til Lungemedicin Næstved')
+
+    # --------- LEGEND ---------
+    leg = plt.legend(fancybox=True, loc='upper center', prop={'size': Fsize / 1.0}, ncol=2, numpoints=1,
+                     bbox_to_anchor=(0.5, 1.12), )  # add the legend
+    leg.get_frame().set_alpha(0.7)
+    # --------------------------
+
+    plt.savefig(plotdir+plotname)
+    plt.clf()
+    plt.close('all')
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    plotname = 'visitationcounts_movingsum.pdf'
+    if verbose: print(' - Initiating ' + plotname)
+
+    fig = plt.figure(figsize=(9, 6))
+    fig.subplots_adjust(wspace=0.1, hspace=0.1, left=0.1, right=0.97, bottom=0.10, top=0.90)
+    Fsize = 10
+    lthick = 2
+    marksize = 4
+    plt.rc('text', usetex=False)
+    plt.rc('font', family='serif', size=Fsize)
+    plt.rc('xtick', labelsize=Fsize)
+    plt.rc('ytick', labelsize=Fsize)
+    plt.clf()
+    plt.ioff()
+    # plt.title('Henvisninger til lungemed. NAE',fontsize=Fsize)
+
+    xvalues = df_results['dates_' + str(measurehour)]
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.YearLocator())
+
+    xerr = None
+    yerr = None
+
+    plt.grid(linestyle=':', linewidth=lthick / 2.)
+    # --------- RANGES ---------
+    ymin = 0.0
+    ymax = 250.0
+    dy = ymax - ymin
+    plt.ylim([ymin, ymax])
+
+    # --------- COLORMAP ---------
+    cmap = plt.cm.get_cmap('viridis')
+    # cmap = plt.cm.get_cmap('plasma')
+    cmin = 0
+    cmax = 100
+
+    colnorm = matplotlib.colors.Normalize(vmin=cmin, vmax=cmax)
+    cmaparr = np.linspace(cmin, cmax, num=50)
+    m = plt.cm.ScalarMappable(cmap=cmap)
+    m.set_array(cmaparr)
+    # cb = plt.colorbar(m)
+    # cb.set_label('belægningsprocent')
+
+    # --------- CALC MOVING SUM ---------
+    Ndayssum = 30
+    df_results['count_vis_aka_23_movingsum'] = df_results['count_vis_aka_23'].rolling(window=Ndayssum).sum()
+    df_results['count_vis_lungNAE_23_movingsum'] = df_results['count_vis_lungNAE_23'].rolling(window=Ndayssum).sum()
+    df_results['count_vis_lungSLA_23_movingsum'] = df_results['count_vis_lungSLA_23'].rolling(window=Ndayssum).sum()
+    df_results['count_vis_other_23_movingsum'] = df_results['count_vis_other_23'].rolling(window=Ndayssum).sum()
+
+    # --------- POINT AND CURVES ---------
+    pointcolor = cmap(colnorm(10))
+    plt.errorbar(xvalues, df_results['count_vis_aka_23_movingsum'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Akut afdelingen (glidende sum over '+str(Ndayssum)+' dage)')
+
+    pointcolor = cmap(colnorm(35))
+    plt.errorbar(xvalues, df_results['count_vis_lungNAE_23_movingsum'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Lungemed NAE (egen afd.; glidende sum over '+str(Ndayssum)+' dage)')
+
+    pointcolor = cmap(colnorm(60))
+    plt.errorbar(xvalues, df_results['count_vis_lungSLA_23_movingsum'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Lungemde. SLA (glidende sum over '+str(Ndayssum)+' dage)')
+
+    pointcolor = cmap(colnorm(85))
+    plt.errorbar(xvalues, df_results['count_vis_other_23_movingsum'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Andre afdelinger (glidende sum over '+str(Ndayssum)+' dage)')
+
+    lineymin = ymin + dy * 0.24
+    lineymax = ymin + dy * 0.40
+    textymin = ymin + dy * 0.05
+    textymax = ymin + dy * 0.10
+    plt.plot(
+        [datetime.datetime.strptime("10-06-2021", "%d-%m-%Y"), datetime.datetime.strptime("10-06-2021", "%d-%m-%Y")],
+        [lineymin, lineymax], '-', color='gray', lw=lthick, zorder=5)
+    plt.text(datetime.datetime.strptime("10-06-2021", "%d-%m-%Y"), textymin, '10-06-2021', fontsize=Fsize, rotation=90,
+             color='gray',
+             horizontalalignment='center', verticalalignment='bottom')
+
+    plt.plot(
+        [datetime.datetime.strptime("01-03-2021", "%d-%m-%Y"), datetime.datetime.strptime("01-03-2021", "%d-%m-%Y")],
+        [lineymin, lineymax], '-', color='gray', lw=lthick, zorder=5)
+    plt.text(datetime.datetime.strptime("01-03-2021", "%d-%m-%Y"), textymin, '01-03-2021', fontsize=Fsize, rotation=90,
+             color='gray',
+             horizontalalignment='center', verticalalignment='bottom')
+
+    # --------- LABELS ---------
+    plt.xlabel('Dato for måling')
+    plt.ylabel('Henvisninger til Lungemedicin Næstved')
+
+    # --------- LEGEND ---------
+    leg = plt.legend(fancybox=True, loc='upper center', prop={'size': Fsize / 1.0}, ncol=2, numpoints=1,
+                     bbox_to_anchor=(0.5, 1.12), )  # add the legend
+    leg.get_frame().set_alpha(0.7)
+    # --------------------------
+
+    plt.savefig(plotdir + plotname)
+    plt.clf()
+    plt.close('all')
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     #Figure plotting number of visitations as a function of bed occupancy; trends between these?
+    plotname = 'visitationsVSoccupancy_available.pdf'
+    if verbose: print(' - Initiating '+plotname)
+
+    fig = plt.figure(figsize=(9, 6))
+    fig.subplots_adjust(wspace=0.1, hspace=0.1, left=0.1, right=0.97, bottom=0.10, top=0.90)
+    Fsize = 10
+    lthick = 2
+    marksize = 4
+    plt.rc('text', usetex=False)
+    plt.rc('font', family='serif', size=Fsize)
+    plt.rc('xtick', labelsize=Fsize)
+    plt.rc('ytick', labelsize=Fsize)
+    plt.clf()
+    plt.ioff()
+    # plt.title(inforstr[:-2],fontsize=Fsize)
+
+    xerr = None
+    yerr = None
+
+    plt.grid(linestyle=':', linewidth=lthick/2.)
+    # --------- RANGES ---------
+    ymin = 0.0
+    ymax = 130.0
+    dy   = ymax - ymin
+    plt.ylim([ymin,ymax])
+
+    # --------- COLORMAP ---------
+    cmap = plt.cm.get_cmap('viridis')
+    #cmap = plt.cm.get_cmap('plasma')
+    cmin = 0
+    cmax = 100
+
+    colnorm = matplotlib.colors.Normalize(vmin=cmin, vmax=cmax)
+    cmaparr = np.linspace(cmin, cmax, num=50)
+    m = plt.cm.ScalarMappable(cmap=cmap)
+    m.set_array(cmaparr)
+    #cb = plt.colorbar(m)
+    #cb.set_label('belægningsprocent')
+
+    # --------- POINT AND CURVES ---------
+    plt.errorbar(df_results['count_vis_aka_23_movingsum'],     df_results['occupancy_actual_movingavg_23'], xerr=xerr, yerr=yerr,
+                 marker='o', lw=0, markersize=marksize, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label='Akut afdelingen (glidende sum over '+str(Ndayssum)+' dage)')
+
+    # --------- LABELS ---------
+    plt.xlabel('Henvisninger til Lungemedicin Næstved')
+    plt.ylabel('Belægningsprocent Lungemedicin Næstved (kl. 23)')
+
+    # --------- LEGEND ---------
+    leg = plt.legend(fancybox=True, loc='upper center', prop={'size': Fsize / 1.0}, ncol=2, numpoints=1,
+                     bbox_to_anchor=(0.5, 1.12), )  # add the legend
+    leg.get_frame().set_alpha(0.7)
+    # --------------------------
+
+    plt.savefig(plotdir+plotname)
+    plt.clf()
+    plt.close('all')
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
