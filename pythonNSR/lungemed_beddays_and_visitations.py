2b2446454e62d1ba1dd7e427f6b9023e818e08fb
Kasper Borello Schmidt <kasperschmidt@me.com>
2022-01-12 14:54:09 +0100

    Plotting beddays

--- pythonNSR/lungemed_beddays_and_visitations.py
+++ pythonNSR/lungemed_beddays_and_visitations.py
@@ -180,6 +180,109 @@ def count_occurrences_per_day(measurehours=[8,15,23], untiltoday=False, savedata
             gdf.savefile(df_results, savepath + filename, format='csv', overwrite=overwrite, verbose=verbose)
 
     return df_results
+# -----------------------------------------------------------------------------------------------------------------------
+def plot_beddays(loaddatafile='lungemedLPR3_SQLbeddays.xlsx', verbose=True,
+                 plotdir='O:\Administration\\02 - Økonomi og PDK\Medarbejdermapper\Kasper\Focus1 - Ad hoc opgaver\Lungemed sengedage og visitationer\plots\\'):
+    """
+    """
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    df_results = gdf.loadExcel(plotdir+loaddatafile)
+    df_results = df_results.sort_values('INDTIDSPUNKT_DRGKONTAKT')
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    plotname = 'beddays_stat.pdf'
+    if verbose: print(' - Initiating '+plotname)
+
+    fig = plt.figure(figsize=(9, 6))
+    fig.subplots_adjust(wspace=0.1, hspace=0.1, left=0.1, right=0.97, bottom=0.10, top=0.90)
+    Fsize = 10
+    lthick = 2
+    marksize = 4
+    plt.rc('text', usetex=False)
+    plt.rc('font', family='serif', size=Fsize)
+    plt.rc('xtick', labelsize=Fsize)
+    plt.rc('ytick', labelsize=Fsize)
+    plt.clf()
+    plt.ioff()
+    # plt.title(inforstr[:-2],fontsize=Fsize)
+
+    xvalues = df_results['INDTIDSPUNKT_DRGKONTAKT']
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.YearLocator())
+
+    xerr = None
+    yerr = None
+
+    plt.grid(linestyle=':', linewidth=lthick/2.)
+    # --------- RANGES ---------
+    ymin = 0.8
+    ymax = 150.0
+    dy   = ymax - ymin
+    plt.ylim([ymin,ymax])
+
+    #plt.xscale('log')
+    plt.yscale('log')
+
+    # --------- COLORMAP ---------
+    cmap = plt.cm.get_cmap('viridis')
+    cmin = 0
+    cmax = 100
+
+    colnorm = matplotlib.colors.Normalize(vmin=cmin, vmax=cmax)
+    cmaparr = np.linspace(cmin, cmax, num=50)
+    m = plt.cm.ScalarMappable(cmap=cmap)
+    m.set_array(cmaparr)
+    #cb = plt.colorbar(m)
+    #cb.set_label('belægningsprocent')
+
+    # --------- POINT AND CURVES ---------
+    pointcolor = cmap(colnorm(40))
+    plt.errorbar(xvalues, df_results['KONTAKTDAGE'], xerr=xerr, yerr=yerr,
+                 marker='o', lw=0, markersize=marksize, alpha=0.5, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=10.)
+
+    Ndaysavg = 60
+    df_results['KONTAKTDAGE_movingavg'] = df_results['KONTAKTDAGE'].rolling(window=Ndaysavg).mean()
+    df_results['KONTAKTDAGE_movingavg_std'] = df_results['KONTAKTDAGE'].rolling(window=Ndaysavg).std()
+
+    plt.errorbar(xvalues, df_results['KONTAKTDAGE_movingavg'], xerr=xerr, yerr=yerr,
+                 marker='.', lw=lthick, markersize=0, alpha=1.0, color=pointcolor,
+                 markerfacecolor=pointcolor, ecolor=pointcolor,
+                 markeredgecolor=pointcolor, zorder=20.,
+                 label=str(Ndaysavg)+' dage glidende gennemsnit med standard afvigelse')
+
+    ylow  = df_results['KONTAKTDAGE_movingavg']-df_results['KONTAKTDAGE_movingavg_std']
+    yhigh = df_results['KONTAKTDAGE_movingavg']+df_results['KONTAKTDAGE_movingavg_std']
+    plt.fill_between(xvalues, ylow, yhigh, color=pointcolor, alpha=0.3, zorder=15)
+
+    lineymin = ymin + dy*0.24
+    lineymax = ymin + dy*0.40
+    textymin = ymin + dy*0.05
+    textymax = ymin + dy*0.10
+    plt.plot([datetime.datetime.strptime("10-06-2021", "%d-%m-%Y"), datetime.datetime.strptime("10-06-2021", "%d-%m-%Y")],
+             [lineymin, lineymax], '-', color='gray', lw=lthick, zorder=5)
+    plt.text(datetime.datetime.strptime("10-06-2021", "%d-%m-%Y"), textymin, '10-06-2021', fontsize=Fsize, rotation=90, color='gray',
+             horizontalalignment='center', verticalalignment='bottom')
+
+    plt.plot([datetime.datetime.strptime("01-03-2021", "%d-%m-%Y"), datetime.datetime.strptime("01-03-2021", "%d-%m-%Y")],
+             [lineymin, lineymax], '-', color='gray', lw=lthick, zorder=5)
+    plt.text(datetime.datetime.strptime("01-03-2021", "%d-%m-%Y"), textymin, '01-03-2021', fontsize=Fsize, rotation=90, color='gray',
+             horizontalalignment='center', verticalalignment='bottom')
+
+    # --------- LABELS ---------
+    plt.xlabel('Dato for måling')
+    plt.ylabel('Kontaktdage Lungemd. NAE')
+
+    # --------- LEGEND ---------
+    leg = plt.legend(fancybox=True, loc='upper center', prop={'size': Fsize / 1.0}, ncol=2, numpoints=1,
+                     bbox_to_anchor=(0.5, 1.12), )  # add the legend
+    leg.get_frame().set_alpha(0.7)
+    # --------------------------
+
+    plt.savefig(plotdir+plotname)
+    plt.clf()
+    plt.close('all')
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # -----------------------------------------------------------------------------------------------------------------------
 def plot_perday_occupancy(measurehours=[23], loaddatafile='lungemedLPR3dataframe.xlsx', verbose=True, untiltoday=True, plotdir='O:\Administration\\02 - Økonomi og PDK\Medarbejdermapper\Kasper\Focus1 - Ad hoc opgaver\Lungemed sengedage og visitationer\plots\\'):
@@ -722,6 +825,12 @@ def plot_perday_occupancy(measurehours=[23], loaddatafile='lungemedLPR3dataframe
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Function loading SP data and plotting it against the LPR3 data for comparison.
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # Predictive time series calculations...
+    # https://machinelearningmastery.com/how-to-grid-search-triple-exponential-smoothing-for-time-series-forecasting-in-python/
+    # https://machinelearningmastery.com/time-series-forecasting-methods-in-python-cheat-sheet/
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     if verbose: print('   Plots saved to \n   ' + plotdir)
