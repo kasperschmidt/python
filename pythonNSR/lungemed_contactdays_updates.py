c6414ac3f41c4331af077c1d3e38d04981a23e8d
Kasper Schmidt <kasperschmidt@me.com>
2022-01-27 15:04:00 +0100

    Histograms of diagnoses and bed days

--- pythonNSR/lungemed_contactdays_updates.py
+++ pythonNSR/lungemed_contactdays_updates.py
@@ -40,7 +40,7 @@ def load_dataframes_from_excel(verbose=True):
         df_update = pd.read_excel(file_update, sheet_name='data output')
         updates_framelist = updates_framelist + [df_update]
 
-    df_updates = pd.concat(updates_framelist)
+    df_updates = pd.concat(updates_framelist, ignore_index=True)
 
     if verbose: print(' - returning dataframes for baseline and updates')
     return df_baseline, df_updates
@@ -234,7 +234,7 @@ def evaluate_beddays_boxplot(datemin_mostrecent='01-01-2022',verbose=True):
     xerr = None
     yerr = None
 
-    plt.grid(linestyle=':', linewidth=lthick/2.)
+    plt.grid(linestyle=':', linewidth=lthick/2., zorder=100, alpha=1.0, color='gray')
     # --------- RANGES ---------
     ymin = -2
     ymax = 30
@@ -259,9 +259,16 @@ def evaluate_beddays_boxplot(datemin_mostrecent='01-01-2022',verbose=True):
     # --------- POINT AND CURVES ---------
     pointcolor = cmap(colnorm(30))
 
-    flierprops = dict(marker='o', markerfacecolor=pointcolor, markersize=marksize, markeredgecolor=pointcolor, alpha=0.3)
-    ax = seaborn.boxplot(x=xvals, y=yvals, linewidth=lthick, flierprops=flierprops,
-                         color=pointcolor)#, labels=, boxprops=dict(alpha=0.5))
+    zorder_boxplot = 10
+    flierprops   = dict(marker='o', markerfacecolor=pointcolor, markersize=marksize, markeredgecolor=pointcolor, alpha=0.3)
+    boxprops     = dict(linewidth=lthick/2, color=pointcolor, facecolor=pointcolor, zorder=zorder_boxplot)
+    whiskerprops = dict(linewidth=lthick/2, color=pointcolor, zorder=zorder_boxplot)
+    medianprops  = dict(linewidth=lthick/2, color=pointcolor, zorder=zorder_boxplot)
+    capprops     = dict(linewidth=lthick/2, color=pointcolor, zorder=zorder_boxplot)
+    ax = seaborn.boxplot(x=xvals, y=yvals, linewidth=lthick, showfliers=True,
+                         flierprops=flierprops, boxprops=boxprops, whiskerprops=whiskerprops,
+                         medianprops=medianprops, capprops=capprops,
+                         color=pointcolor)
     for patch in ax.artists:
         r, g, b, a = patch.get_facecolor()
         patch.set_facecolor((r, g, b, 0.5))
@@ -298,7 +305,181 @@ def evaluate_beddays_boxplot(datemin_mostrecent='01-01-2022',verbose=True):
     if verbose: print(' - saved plot to '+plotdir)
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    #Locate the indices for the subsamples?? loc[pd.date_range(ts.index.min(), ts.index.max(), freq='M')]
+# -----------------------------------------------------------------------------------------------------------------------
+def beddays_distributions(datemin_mostrecent = '01-01-2022', normalization="probability", verbose=True):
+    """
+    lcu.beddays_distributions(normalization="probability")
+    lcu.beddays_distributions(normalization="count")
+    """
+    plotdir = 'O:/Administration/02 - Økonomi og PDK/Medarbejdermapper/Kasper/Focus1 - Ad hoc opgaver/Lungemed sengedage og visitationer/plots/'
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    df_baseline, df_updates = lcu.load_dataframes_from_excel()
+
+    datemin     = datetime.datetime.strptime(datemin_mostrecent, "%d-%m-%Y")
+    dropval     = np.where(df_updates['Kontakt startdato Dato-tid'] < datemin)[0]
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    plotname = 'distributions_days_'+normalization+'.pdf'
+    if verbose: print(' - Initiating '+plotname)
+
+    fig = plt.figure(figsize=(6, 6))
+    fig.subplots_adjust(wspace=0.1, hspace=0.1, left=0.12, right=0.98, bottom=0.10, top=0.98)
+    Fsize = 10
+    lthick = 2
+    marksize = 4
+    plt.rc('text', usetex=False)
+    plt.rc('font', family='serif', size=Fsize)
+    plt.rc('xtick', labelsize=Fsize)
+    plt.rc('ytick', labelsize=Fsize)
+    plt.clf()
+    plt.ioff()
+    # plt.title(inforstr[:-2],fontsize=Fsize)
+
+    # --------- RANGES ---------
+    xmin = 0
+    xmax = 40
+    dy   = xmax - xmin
+    plt.xlim([xmin,xmax])
+
+    plt.grid(linestyle=':', linewidth=lthick/2.)
+
+    # --------- COLORMAP ---------
+    cmap = plt.cm.get_cmap('viridis')
+    cmin = 0
+    cmax = 100
+
+    colnorm = matplotlib.colors.Normalize(vmin=cmin, vmax=cmax)
+    cmaparr = np.linspace(cmin, cmax, num=50)
+    m = plt.cm.ScalarMappable(cmap=cmap)
+    m.set_array(cmaparr)
+
+    # --------- POINT AND CURVES ---------
+    distcolor = cmap(colnorm(30))
+    seaborn.histplot(data=df_baseline, x='KONTAKTDAGE', color=distcolor, alpha=0.3,
+                     kde=True, binwidth=1, discrete=True, stat=normalization, common_norm=False,
+                     label='Baseline (2019-2021)')
+
+    distcolor = cmap(colnorm(70))
+    seaborn.histplot(data=df_updates, x='Forskel på kontakt start og slut (antal dage)', color=distcolor, alpha=0.3,
+                     kde=True, binwidth=1, discrete=True, stat=normalization, common_norm=False,
+                     label='Forløb siden 01-12-2021')
+
+    distcolor = 'black'
+    seaborn.histplot(data=df_updates.drop(df_baseline.index[dropval]), x='Forskel på kontakt start og slut (antal dage)', color=distcolor, alpha=0.3,
+                     kde=True, binwidth=1, discrete=True, stat=normalization, common_norm=False,
+                     label='Forløb siden '+datemin_mostrecent)
+
+    # --------- LABELS ---------
+    plt.xlabel('Kontaktdage')
+    ylabelstr = 'Antal forløb '
+    if normalization == 'probability':
+        ylabelstr = ylabelstr+'(brøkdel af samlede antal forløb)'
+    plt.ylabel(ylabelstr)
+
+    # --------- LEGEND ---------
+    leg = plt.legend(fancybox=True, loc='upper center', prop={'size': Fsize / 1.0}, ncol=1, numpoints=1,
+                     bbox_to_anchor=(0.7, 0.95))  # add the legend
+    leg.get_frame().set_alpha(0.7)
+    # --------------------------
+
+    plt.savefig(plotdir+plotname)
+    plt.clf()
+    plt.close('all')
+    if verbose: print(' - saved plot to '+plotdir)
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# -----------------------------------------------------------------------------------------------------------------------
+def diagnoses_distributions(datemin_mostrecent = '01-01-2022', normalization="probability",verbose=True):
+    """
+    lcu.diagnoses_distributions()
+    """
+    plotdir = 'O:/Administration/02 - Økonomi og PDK/Medarbejdermapper/Kasper/Focus1 - Ad hoc opgaver/Lungemed sengedage og visitationer/plots/'
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    df_baseline, df_updates = lcu.load_dataframes_from_excel()
+    df_baseline['DIA01'] = pd.Categorical(df_baseline['DIA01'], np.sort(np.unique(np.asarray(df_baseline['DIA01']))))
+
+    datemin     = datetime.datetime.strptime(datemin_mostrecent, "%d-%m-%Y")
+    dropval     = np.where(df_updates['Kontakt startdato Dato-tid'] < datemin)[0]
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    plotname = 'distributions_diagnoses_'+normalization+'.pdf'
+    if verbose: print(' - Initiating '+plotname)
+
+    fig = plt.figure(figsize=(11, 5))
+    fig.subplots_adjust(wspace=0.1, hspace=0.1, left=0.12, right=0.98, bottom=0.15, top=0.98)
+    Fsize = 10
+    lthick = 2
+    marksize = 4
+    plt.rc('text', usetex=False)
+    plt.rc('font', family='serif', size=Fsize)
+    plt.rc('xtick', labelsize=Fsize)
+    plt.rc('ytick', labelsize=Fsize)
+    plt.clf()
+    plt.ioff()
+    # plt.title(inforstr[:-2],fontsize=Fsize)
+
+    # --------- RANGES ---------
+    xmin = 0
+    xmax = 40
+    dy   = xmax - xmin
+    #plt.xlim([xmin,xmax])
+
+    #plt.grid(linestyle=':', linewidth=lthick/2.)
+
+    # --------- COLORMAP ---------
+    cmap = plt.cm.get_cmap('viridis')
+    cmin = 0
+    cmax = 100
+
+    colnorm = matplotlib.colors.Normalize(vmin=cmin, vmax=cmax)
+    cmaparr = np.linspace(cmin, cmax, num=50)
+    m = plt.cm.ScalarMappable(cmap=cmap)
+    m.set_array(cmaparr)
+
+    # --------- POINT AND CURVES ---------
+    distcolor = cmap(colnorm(30))
+    seaborn.histplot(data=df_baseline, x='DIA01', color=distcolor, alpha=0.3,
+                     kde=True, binwidth=1, discrete=True, stat=normalization, common_norm=False,
+                     label='Baseline (2019-2021)')
+
+    distcolor = cmap(colnorm(70))
+    seaborn.histplot(data=df_updates, x='Aktionsdiagnosekode', color=distcolor, alpha=0.3,
+                     kde=True, binwidth=1, discrete=True, stat=normalization, common_norm=False,
+                     label='Forløb siden 01-12-2021')
+
+    distcolor = 'black'
+    seaborn.histplot(data=df_updates.drop(df_baseline.index[dropval]), x='Aktionsdiagnosekode', color=distcolor, alpha=0.3,
+                     kde=True, binwidth=1, discrete=True, stat=normalization, common_norm=False,
+                     label='Forløb siden '+datemin_mostrecent)
+
+    # --------- LABELS ---------
+    plt.xticks(rotation=90, fontsize=1.4)
+    plt.xlabel('Kontaktdage')
+    ylabelstr = 'Antal forløb med diagnose '
+    if normalization == 'probability':
+        ylabelstr = ylabelstr+'(brøkdel af samlede antal forløb)'
+    plt.ylabel(ylabelstr)
+
+    # --------- LEGEND ---------
+    leg = plt.legend(fancybox=True, loc='upper center', prop={'size': Fsize / 1.0}, ncol=1, numpoints=1,
+                     bbox_to_anchor=(0.7, 0.95))  # add the legend
+    leg.get_frame().set_alpha(0.7)
+    # --------------------------
+
+    plt.savefig(plotdir+plotname)
+    plt.clf()
+    plt.close('all')
+    if verbose: print(' - saved plot to '+plotdir)
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+def heatmap_diagnosisVSbeddays(verbose=True):
+    """
+    lcu.beddays_distributions()
+    """
+
 
 #=======================================================================================================================
 
