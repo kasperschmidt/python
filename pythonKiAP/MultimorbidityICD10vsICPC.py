a279998c14cf2b6849101789b254b23626bf72bf
Kasper Schmidt <kbschmidt@kiap.dk>
2025-01-20 14:30:16 +0100

    Add multimorbidity column to mapping dataframe and call mapping on a few examples from main function

--- pythonKiAP/MultimorbidityICD10vsICPC.py
+++ pythonKiAP/MultimorbidityICD10vsICPC.py
@@ -20,15 +20,23 @@ def main(verbose=True):
         
     print(" - Defining multimorbidity classes for ICPC based on ICD10 calssification in Schiøtz et al. BMC Public Health (2017) 17:422")
 
-    df_ICPCICD10 = mmii.load_ICPC_ICD10_mapping(verbose=verbose)
+    map_ICPCICD10 = mmii.load_ICPC_ICD10_mapping(verbose=verbose)
+    map_ICPCICD10 = mmii.add_multimorbitity_column(map_ICPCICD10,verbose=verbose)
 
-    diaarr = mmii.map_ICD10toICPC('dr520',ICD10in=True,verbose=verbose)
-    print(' - The returned mapping: \n'+str(diaarr))
-    print(diaarr.dtype)
+    ICD10_list = ['DM15','de10']
+    for ii, ICD10 in enumerate(ICD10_list):
+        print(' - Looking op map and multimorbidity category for : '+ICD10)
+        diagnosis_mapping, multimorbidity_category = mmii.map_ICD10toICPC(ICD10,map_ICPCICD10,ICD10in=True,verbose=verbose)
+        print('   The returned mapping:     \n'+str(diagnosis_mapping))
+        print('   The returned mm category: \n'+str(multimorbidity_category))
 
-    diaarr = mmii.map_ICD10toICPC('A01',ICD10in=False,verbose=verbose)
-    print(' - The returned mapping: \n'+str(diaarr))
-    print(diaarr.dtype)
+    ICPC_list = ['A01','b80','T89']
+    for jj, ICPC in enumerate(ICPC_list):
+        print(' - Looking op map and multimorbidity category for : '+ICPC)
+        diagnosis_mapping, multimorbidity_category = mmii.map_ICD10toICPC(ICPC,map_ICPCICD10,ICD10in=False,verbose=verbose)
+        print('   The returned mapping:     \n'+str(diagnosis_mapping))
+        print('   The returned mm category: \n'+str(multimorbidity_category))
+    
 
 #--------------------------------------------------------------------------------------------------------------------
 def map_ICD10toICPC(diagnosis,map_ICPCICD10,ICD10in=True,verbose=True):
@@ -44,44 +52,80 @@ def map_ICD10toICPC(diagnosis,map_ICPCICD10,ICD10in=True,verbose=True):
 
     """
     if ICD10in:
-        in_col  = 'icd10'
-        out_col = 'icpc2'
+        diagnosis = diagnosis.lower()
+        if len(diagnosis) == 3:
+            in_col    = 'icd10_level3'
+        elif len(diagnosis) == 4:
+            in_col    = 'icd10_level4'
+        elif len(diagnosis) == 5:
+            in_col    = 'icd10_level5'
+        else:
+            in_col    = 'icd10'
+        out_col   = 'icpc2'
     else:
-        in_col  = 'icpc2'
-        out_col = 'icd10'
+        in_col    = 'icpc2'
+        out_col   = 'icd10'
+        diagnosis = diagnosis.upper()
     
     goodent = np.where(map_ICPCICD10[in_col] == diagnosis)[0]
     if len(goodent) > 0:
-        dialist = map_ICPCICD10[out_col][goodent]
+        diagnosis_mapping       = map_ICPCICD10.loc[goodent,out_col]
+        multimorbidity_category = map_ICPCICD10.loc[goodent,'multimobidity_category']
     else:
         if verbose: print(' - WARNING: There were not matches to the input diagnosis (ICD10in='+str(ICD10in)+'):'+in_col)
-        dialist = np.asarray([])
-
-    return dialist
+        diagnosis_mapping       = np.asarray([])
+        multimorbidity_category = np.asarray([])
+    
+    return diagnosis_mapping, multimorbidity_category
 #--------------------------------------------------------------------------------------------------------------------
 def load_ICPC_ICD10_mapping(verbose=True):
     """
     Function loading overall ICPC vs ICD10 code mapping
     """
     if verbose: print(' - Loading ICPC-ICD10 mapping from SQL database')
-    SQLquery = "SELECT TOP 1000 * FROM opslagsdata.dbo.icpc_icd10_mapning"
+    SQLquery = "SELECT * FROM opslagsdata.dbo.icpc_icd10_mapning"
     server='10.19.31.20' 
     database='opslagsdata'
     savefilename=None
     overwrite=False
     df_map = kku.returnSQLdatapull(SQLquery,server=server,database=database,savefilename=savefilename,overwrite=overwrite,verbose=verbose)
 
-    if verbose: print(' - Aadding convenience columns to dataframe (ICD10 leve 3, 4 and 5)')
+    if verbose: print(' - Aadding convenience columns to dataframe (ICD10 level 3, 4 and 5)')
     df_map['icd10_level3'] = df_map['icd10'].str.slice(0,3)
     df_map['icd10_level4'] = df_map['icd10'].str.slice(0,4)
     df_map['icd10_level5'] = df_map['icd10'].str.slice(0,5)
 
     return df_map
 #--------------------------------------------------------------------------------------------------------------------
+def add_multimorbitity_column(map_ICPCICD10,verbose=True):
+    """
+    Function adding column with multimorbidity category to ICPC-ICD10 mapping dataframe
+    """
+    if verbose: print(' - Looping over ICD10 codes to add multimorbidity classification ')
+    unique_codes = np.unique(map_ICPCICD10['icd10'].dropna())
+
+    map_ICPCICD10['multimobidity_category']        = map_ICPCICD10['id']*0.0 # dummy column to be filled containing 0's and Nones from ICD10 column
+    map_ICPCICD10['multimobidity_category_text']   = map_ICPCICD10['icd10'].str.slice(0,1) # dummy column to be filled containing d's and Nones from ICD10 column
+    
+    for ii, ucode in enumerate(unique_codes):
+        goodent = np.where(map_ICPCICD10['icd10'] == ucode)[0]
+        if ucode is None:
+            map_ICPCICD10.loc[goodent,'multimobidity_category']         = None
+            map_ICPCICD10.loc[goodent,'multimobidity_category_text']    = None
+
+        else:
+            diagroup = mmii.multimorbidity_diagnosis_grouping_ICD10(ucode)
+            map_ICPCICD10.loc[goodent,'multimobidity_category']         = diagroup[0]
+            map_ICPCICD10.loc[goodent,'multimobidity_category_text']    = diagroup[1]
+
+    return map_ICPCICD10
+#--------------------------------------------------------------------------------------------------------------------
 def multimorbidity_diagnosis_grouping_ICD10(ICD10code):
     """
-    Cronical diagnosis groups following Schiøtz et al. BMC Public Health (2017) 17:422
+    Cronical diagnosis groups following Tabel 1 of Schiøtz et al. BMC Public Health (2017) 17:422.
+    As the diabetes group is not given ICD10 codes in Schiøtz et al. they are manually added.
     """
+    ICD10code = ICD10code.upper()
     # Diabetes
     if ICD10code[:4] in ('DE10','DE11','DE12','DE13','DE14'):
         diagroup = [1,'Diabetes']
